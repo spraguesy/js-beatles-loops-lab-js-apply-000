cde94bcedb592a0bac8a3c63eb3d250d51f729a9
Andrew Sprague <andrewsprague@outlook.com>
2018-08-02 03:18:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ function theBeatlesPlay (musicians, instruments) {
   let x = [];
   
   for (let i = 0; i < musicians.length; i++) {
-    let sentence 
+    let sentence = `${musicians[i]} plays ${instruments[i]}`;
+    x.push(sentence);
   }
+}
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i < facts.length; i++) {
+    let newString = facts[i] + '!!!';
+    facts[i] = newString;
+  }
+  
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  let arr = [];
+  
+  do {
+    arr.push("I love the Beatles!");
+    n++;
+  } while (n < 15)
+  
+  return arr;
+    
 }
